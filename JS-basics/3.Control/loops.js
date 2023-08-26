7929e571166a9187d48028dde462c51438d25c4c
kunwarx08 <kunwardeep.s.08@gmail.com>
2023-08-26 16:00:43 +0530

    Doing Control Flow

---
+++ JS-basics/3.Control/loops.js
@@ -0,0 +1,33 @@
+// Loops
+
+//for loop
+console.log("for loop");
+for(let i=1;i<=10;i++)
+{
+    if (i % 2 !== 0) //only odd numbers will be printed
+    {console.log(i);} 
+    
+}
+
+//while loop
+console.log("while loop");
+let i=1;
+while(i<=10)
+{
+    if (i % 2 !== 0) //only odd numbers will be printed
+    {console.log(i);} 
+    i++;
+}
+
+//do while loop
+console.log("do-while loop"); //rarely used
+let j=1;
+do{
+    
+    if (j % 2 !== 0) //only odd numbers will be printed
+    {console.log(j);} 
+    j++;
+}while(j<=10);
+
+
+
