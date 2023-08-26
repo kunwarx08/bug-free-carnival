aa9e02f4008b4a27d74d072ea900a2e279cd96bf
kunwarx08 <kunwardeep.s.08@gmail.com>
2023-08-26 16:30:36 +0530

    Finished loops

--- JS-basics/3.Control/loops.js
+++ JS-basics/3.Control/loops.js
@@ -9,6 +9,10 @@ for(let i=1;i<=10;i++)
     
 }
 
+
+///////////////////////////////////////////////////////////
+
+
 //while loop
 console.log("while loop");
 let i=1;
@@ -18,6 +22,10 @@ while(i<=10)
     {console.log(i);} 
     i++;
 }
+
+
+///////////////////////////////////////////////////////////
+
 
 //do while loop
 console.log("do-while loop"); //rarely used
@@ -30,4 +38,43 @@ do{
 }while(j<=10);
 
 
+//////////////////////////////////////////////////////////
 
+
+
+//for-in loop
+console.log("for-in loop");
+const person={
+    name:"John",
+    age:30
+};
+
+//sytax for for-in loop
+//for(let loop_variable in object)
+for(let key in person) //we use for in loop to iterate over the properties of an object 
+{
+    console.log(key,person[key]); //key is the property of the object
+    //person[key] is the value of the property
+    //we used bracket notation
+}
+
+//similarly iterating arrays using for-in loop
+
+const colors=["red","green","blue"];
+for(let index in colors)
+{
+    console.log(index,colors[index]);
+}
+
+
+//for-of loop
+
+console.log("for-of loop");
+//for-of loop is used to iterate over the elements of an array
+//sytax for for-of loop
+//for(let loop_variable of array)
+const cars=["bmw","audi","suzuki"];
+for(let car of cars)
+{
+    console.log(car);
+}
