686b7280c483262337b611f693871b50261bf2a6
kunwarx08 <kunwardeep.s.08@gmail.com>
2023-08-26 18:47:42 +0530

    having fun in control  exercises

---
+++ JS-basics/3.Control/Exercise/demerit.js
@@ -0,0 +1,12 @@
+
+
+checkSpeed(80);
+
+function checkSpeed(speed)
+{
+    if(speed <= 70)
+    console.log('OK');
+    if(speed>70){
+        console.log('1 point')
+    }
+}
--- JS-basics/3.Control/Exercise/fizzbuzz.js
+++ JS-basics/3.Control/Exercise/fizzbuzz.js
@@ -1,3 +1,19 @@
-let a=1;
+function fizzBuzz(input)
+{
+    if (typeof input !== 'number')
+        return 'Not a number'; // alternatively we can write NaN
+    if((input % 3 === 0) && (input % 5 == 0))
+        return 'FizzBuzz';
+    
+    if(input % 3 === 0)
+        return 'Fizz';
+    
+    if(input % 5 === 0)
+        return 'Buzz';
+    
+    return input;
+}
 
-console.log(typeof a);
+
+const output = fizzBuzz(15);
+console.log(output);
